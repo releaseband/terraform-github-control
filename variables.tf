35a6dd206102c88112ab67a643d6200a2ea0e9ee
Vitalii <zebesh.vlad@gmail.com>
2023-04-03 13:37:08 +0700

    feat(variables.tf): add optional teams list to repositories variable feat(variables.tf): add billing_email variable feat(variables.tf): add teams variable as a map of objects with optional members list

--- variables.tf
+++ variables.tf
@@ -4,6 +4,7 @@ variable "repositories" {
     archived     = optional(bool)
     description  = optional(string)
     homepage_url = optional(string)
+    teams        = optional(list(string))
     pages = optional(object({
       branch = optional(string)
       path   = optional(string)
@@ -11,7 +12,19 @@ variable "repositories" {
   }))
 }
 
+
+
+
 variable "members" {
   type = map(string)
 }
 
+variable "billing_email" {
+  type = string
+}
+
+variable "teams" {
+  type = map(object({
+    members = optional(list(string))
+  }))
+}
