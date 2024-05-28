5cc966c10bdd3758ca7fd25c0cf2b740d53cfd1b
Vitalii <zebesh.vlad@gmail.com>
2024-05-28 10:31:15 +0700

    fix: move signoff to repo conf

--- module_global/main.tf
+++ module_global/main.tf
@@ -11,7 +11,7 @@ resource "github_organization_settings" "main" {
   members_can_create_public_pages                              = true
   members_can_create_private_pages                             = true
   members_can_fork_private_repositories                        = false
-  web_commit_signoff_required                                  = true
+  web_commit_signoff_required                                  = false
   advanced_security_enabled_for_new_repositories               = false
   dependabot_alerts_enabled_for_new_repositories               = true
   dependabot_security_updates_enabled_for_new_repositories     = true
